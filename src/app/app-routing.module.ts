1c852d89cdae09daf0f08557b6dd20557277e203
Dark <gabriel.rosa15@outlook.com>
2025-02-13 23:08:17 -0300

    inicio rapido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import {DeleteUserComponent} from "./delete-user/delete-user.component";
+import {ChatComponent} from "./chat/chat.component";
+import {ConversaComponent} from "./conversa/conversa.component";
+import {ListarFuncionariosComponent} from "./listar-funcionarios/listar-funcionarios.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registro', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'deleteUser', component: DeleteUserComponent},
+  { path: 'chats', component: ChatComponent},
+  { path: 'conversa/:titulo', component: ConversaComponent},
+  { path: 'funcionarios', component: ListarFuncionariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { ChatComponent } from './chat/chat.component';
+import { ConversaComponent } from './conversa/conversa.component';
+import { ListarFuncionariosComponent } from './listar-funcionarios/listar-funcionarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomeComponent,
+    LoginComponent,
+    DeleteUserComponent,
+    ChatComponent,
+    ConversaComponent,
+    ListarFuncionariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
